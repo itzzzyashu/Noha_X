8204ff0e630f90cbcf466f9f88a8794d08247b08
AnshNityam <83384892+YashBebe@users.noreply.github.com>
2021-10-08 14:43:51 +0530

    Update chatbot.py

--- Cutiepii_Robot/modules/chatbot.py
+++ Cutiepii_Robot/modules/chatbot.py
@@ -118,7 +118,7 @@ def chatbot(update: Update, context: CallbackContext):
             return
         Message = message.text
         bot.send_chat_action(chat_id, action="typing")
-        kukiurl = requests.get('https://kuki-api.tk/api/NohaX/@itzzzyashu/message='+Message)
+        kukiurl = requests.get('https://kuki-api.tk/api/NohaX/@darkworld_op/message='+Message)
         Kuki = json.loads(kukiurl.text)
         kuki = Kuki['reply']
         sleep(0.3)
